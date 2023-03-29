1653082a360c9981b1a5e48a10adc69b3425eddd
Dimas Capelari <97951537+dimascapelari@users.noreply.github.com>
2023-03-29 16:17:58 -0300

    map() trazendo dados da api para a tela

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -7,14 +7,13 @@ const Home = () => {
   const [posts, setPosts] = useState([]);
 
   const getPosts = async () => {
-    // console.log("testando");
     try {
       const response = await axios.get(
         "https://jsonplaceholder.typicode.com/posts"
       );
       const data = response.data;
-      console.log(data);
-      //   console.log(response);
+
+      setPosts(data);
     } catch (error) {
       console.log(error);
     }
@@ -24,7 +23,25 @@ const Home = () => {
     getPosts();
   }, []);
 
-  return <div>Home</div>;
+  return (
+    <div className="home">
+      <h1>Últimos posts</h1>
+      {/* {posts.length === 0 ? <p>Carregando...</p> : <p>Carregou</p>} */}
+      {posts.length === 0 ? (
+        <p>Carregando...</p>
+      ) : (
+        posts.map((post) => (
+          <div className="post" key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+            <Link to={`/post/${post.id}`} className="btn">
+              Ler mais
+            </Link>
+          </div>
+        ))
+      )}
+    </div>
+  );
 };
 
 export default Home;
